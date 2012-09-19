2ffe950117c2280c5998b4591dff0d126baf68bb
Peter Kalchgruber <pedak@univie>
2012-09-19 15:49:48 +0200

    cleanup merge

--- oai-adapter.py
+++ oai-adapter.py
@@ -4,48 +4,82 @@
 simulate-oai-adapter: The ResourceSync command line tool for OAI-PMH synchronization.
 
 Created by Peter Kalchgruber on 2012-09-01.
+Based on simulate-source of Bernhard Haslhofer
 """
 
-import argparse
+import optparse
 import yaml
 import logging
-import logging.config
-import datetime
-
 
 from resync.source import Source
 from resync.http import HTTPInterface
+from resync.util import UTCFormatter
 
 DEFAULT_CONFIG_FILE = 'config/default.yaml'
-DEFAULT_LOG_FILE = 'config/logging.yaml'
+SOURCE_LOG_FILE = 'resync-source.log'
+
+def init_logging(file=False, console=False, eval_mode=False):
+    """Initialize logging"""
+    
+    fmt = '%(asctime)s | %(name)s | %(levelname)s | %(message)s'
+    formatter = UTCFormatter(fmt)
+    
+    if console:
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+    if file:
+        fh = logging.FileHandler(filename=SOURCE_LOG_FILE, mode='a')
+        fh.setFormatter(formatter)
+    
+    loggers = ['source', 'changememory', 'http', 'sitemap', 
+               'inventory_builder']
+    for logger in loggers:
+        log = logging.getLogger(logger)
+        if eval_mode:
+            log.setLevel(logging.DEBUG)
+        else:
+            log.setLevel(logging.INFO)
+        if console:
+            log.addHandler(ch)
+        if file:
+            log.addHandler(fh)
 
 def main():
     
+    p = optparse.OptionParser(description='ResourceSync source simulator',
+        usage='usage: %prog [options] uri_path local_path  (-h for help)')
+        
     # Define simulator options
-    parser = argparse.ArgumentParser(
-                            description = "ResourceSync OAIPMH-Adapter")
-    parser.add_argument('--config-file', '-c', 
+    p.add_option('--config-file', '-c', type=str, action='store',
                     default=DEFAULT_CONFIG_FILE,
-                    help="the adapter configuration file")
-    parser.add_argument('--log-config', '-l',
-                    default=DEFAULT_LOG_FILE,
-                    help="the logging configuration file")
-    parser.add_argument('--port', '-p', type=int,
+                    help="the simulation configuration file")
+    p.add_option('--port', '-p', type=int, action='store',
                     default=8888,
                     help="the HTTP interface port")
-    parser.add_argument('--hostname', '-n',
+    p.add_option('--hostname', '-n', type=str, action='store',
                     default="localhost",
-                    help="the hostname where the adapter is running")
+                    help="the hostname where the simulator is running")
+    p.add_option('--logger', '-l', action='store_true',
+                    default=False,
+                    help="write detailed logs of source to file")
+    p.add_option('--verbose', '-v', action='store_true',
+                    default=False,
+                    help="verbose")
+    p.add_option('--eval', '-e', action='store_true',
+                    default=False,
+                    help="run in evaluation mode (log all events)")
     
     # Parse command line arguments
-    args = parser.parse_args()
-
-    # Load the logging configuration file and set up logging
-    logconfig = yaml.load(file(args.log_config, 'r'))
-    logging.config.dictConfig(logconfig)
+    (args, map) = p.parse_args()
     
+    if(args.logger or args.verbose):
+        init_logging(file=args.logger, console=args.verbose,
+                     eval_mode=args.eval)
+        
     # Load the YAML configuration file
     config = yaml.load(file(args.config_file, 'r'))
+    
+    print "Setting up ResourceSync source simulator..."
     
     # Set up the source
     source_settings = config['source']
@@ -64,8 +98,6 @@ def main():
         klass_name = config['changememory']['class']
         mod = __import__('resync.changememory', fromlist=[klass_name])
         changemem_klass = getattr(mod, klass_name)
-
-
         changememory = changemem_klass(source, config['changememory'])
         source.add_changememory(changememory)
     
@@ -84,7 +116,9 @@ def main():
         logger = logger_class(source, config['logger'])
     
     # Bootstrap the source
+    print "Bootstrapping the source with initial dataset ..."
     source.bootstrap()
+    print "Source simulator is now running at %s" % source.base_uri
     
     # Start the Web interface, run the simulation
     # Attach HTTP interface to source
@@ -96,9 +130,8 @@ def main():
         http_interface.start()
         #startdate=datetime.datetime(2012,9,12,1,2,3) 
         source.bootstrap_oai(source_settings['endpoint'])
-
     except KeyboardInterrupt:
-        print "Exiting gracefully..."
+        print "\nStopping simulation and exiting gracefully..."
     finally:
         http_interface.stop()
 
